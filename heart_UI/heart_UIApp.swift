ba4ba1f4c97308146a86cc45034195211022114a
Hasegawa-Akito <h.akito1999@gmail.com>
2022-07-13 19:42:47 +0900

    Initial Commit

---
+++ heart_UI/heart_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  heart_UIApp.swift
+//  heart_UI
+//
+//  Created by Hasegawa Akito on 2022/07/13.
+//
+
+import SwiftUI
+
+@main
+struct heart_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
